dabaa8a7474ac0e8a2a3d2d0ee5a53c7ae860001
Finn <tupfi2@gmx.de>
2020-10-14 15:45:18 +0200

    Optimize database format

--- src/utils/database.ts
+++ src/utils/database.ts
@@ -112,9 +112,9 @@ export const getDbResults = async (options: string): Promise<any[]> => {
 const createDefaultTables = (): void => {
     if (!checkDatabaseStatus()) return;
     dbConnection.query(
-        'CREATE TABLE IF NOT EXISTS users (username VARCHAR(10) NOT NULL, salt TEXT NOT NULL, verifier TEXT NOT NULL, public_key TEXT NOT NULL, private_key TEXT NOT NULL, UNIQUE KEY unique_username (username)) ENGINE = InnoDB DEFAULT CHARSET=utf8;');
+        'CREATE TABLE IF NOT EXISTS users (username VARCHAR(10) NOT NULL, salt VARCHAR(64) NOT NULL, verifier VARCHAR(512) NOT NULL, public_key VARCHAR(360) NOT NULL, private_key VARCHAR(3872) NOT NULL, UNIQUE KEY unique_username (username)) ENGINE = InnoDB DEFAULT CHARSET=utf8;');
     dbConnection.query(
-        'CREATE TABLE IF NOT EXISTS logins (username VARCHAR(10) NOT NULL, id int NOT NULL AUTO_INCREMENT, server_ephemeral TEXT NOT NULL, client_ephemeral TEXT NOT NULL, session_proof TEXT, UNIQUE KEY unique_id (id)) ENGINE = InnoDB DEFAULT CHARSET=utf8;');
+        'CREATE TABLE IF NOT EXISTS logins (username VARCHAR(10) NOT NULL, id int NOT NULL AUTO_INCREMENT, server_ephemeral VARCHAR(64) NOT NULL, client_ephemeral VARCHAR(512) NOT NULL, session_proof VARCHAR(64), UNIQUE KEY unique_id (id)) ENGINE = InnoDB DEFAULT CHARSET=utf8;');
     dbConnection.query(
         'CREATE TABLE IF NOT EXISTS categories (id int NOT NULL AUTO_INCREMENT, name TEXT NOT NULL, descirption TEXT NOT NULL, is_split BOOLEAN, UNIQUE KEY unique_id (id)) ENGINE = InnoDB DEFAULT CHARSET=utf8;');
 };
